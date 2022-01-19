d6b5f868690e20fc6900d07788c3955011f39ea7
parulmahant <parulmahant665@gmail.com>
2022-01-19 17:38:59 -0500

    Second Commit with for loop

---
+++ src/newgitproject/newclass1.java
@@ -0,0 +1,17 @@
+package newgitproject;
+
+public class newclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int sum=0;
+	for(int j=0;j<11;j++) {	
+		sum =sum+10;
+	
+	}
+		System.out.println(sum);
+	}
+
+}
