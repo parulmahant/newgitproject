157a69b0c89656adae0aa1b89e85ca6c7634c9f4
parulmahant <parulmahant665@gmail.com>
2022-01-19 16:51:55 -0500

    First Commit 1

---
+++ src/newgitproject/newclass.java
@@ -0,0 +1,22 @@
+package newgitproject;
+
+public class newclass {
+
+	
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		String you="yes";
+		if(you=="yes") {
+			
+			System.out.println("correct");
+		}
+		else {
+			System.out.println("correct");
+			
+		}
+			}
+
+	}
+
+
